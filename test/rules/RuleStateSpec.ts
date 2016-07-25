3b4798d791778e337f5e957efe371e0e6c72c708
Floris Bernard <floris.bernard@gmail.com>
2016-07-25 09:18:54 +0200

    Recover spec file

---
+++ test/rules/RuleStateSpec.ts
@@ -0,0 +1,72 @@
+import * as chai from 'chai';
+import RuleState from "../../src/lib/rules/RuleState";
+import RuleType from "../../src/lib/rules/RuleType";
+const {expect} = chai;
+
+describe('RuleState', () =>
+{
+	describe('validate', () =>
+	{
+		describe('with a rule that takes 30ms to complete', () =>
+		{
+			const rule = new RuleState('test', RuleType.FUNCTION, () => (
+				new Promise<boolean>((resolve) =>
+				{
+					setTimeout(() => resolve(true), 30);
+				})
+			));
+			const validation = rule.validate('foobar');
+			it('should eventually resolve with the result', () =>
+			{
+				expect(validation).to.eventually.equal(true);
+			});
+			it('should initially set isValidating to "true"', () =>
+			{
+				expect(rule.isValidating()).to.equal(true);
+			});
+			it('should return isValidating to false after 30ms', done =>
+			{
+				setTimeout(() =>
+				{
+					expect(rule.isValidating()).to.equal(false);
+					done();
+				}, 60);
+			});
+		});
+		describe('with COLLECTION_AND and two rules that complete at different times', () =>
+		{
+			const rule = new RuleState('test', RuleType.COLLECTION_AND, [
+				new RuleState('subtest1', RuleType.FUNCTION, () => (
+					new Promise<boolean>((resolve) =>
+					{
+						setTimeout(() => resolve(false), 30);
+					})
+				)),
+				new RuleState('subtest2', RuleType.FUNCTION, () => (
+					new Promise<boolean>((resolve) =>
+					{
+						setTimeout(() => resolve(true), 90);
+					})
+				))
+			]);
+			const validation = rule.validate('foobar');
+
+			it('should eventually resolve with the result', () =>
+			{
+				expect(validation).to.eventually.equal(false);
+			});
+			it('should initially set isValidating to "true"', () =>
+			{
+				expect(rule.isValidating()).to.equal(true);
+			});
+			it('should return isValidating to false after both rules complete', done =>
+			{
+				setTimeout(() =>
+				{
+					expect(rule.isValidating()).to.equal(false);
+					done();
+				}, 120);
+			});
+		});
+	});
+});
